070f4732d93359a2ae33806038205744ad195c34
Daniel Ku <kjunine@gmail.com>
2014-09-07 23:05:17 +0900

    Adds environment variable configuration.

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ var connector = require('./connector'),
     checker = require('./checker'),
     configurator = require('./configurator');
 
-var id = 'v4d';
-var master = '192.168.8.10:27017';
-var slaves = ['192.168.8.11:27017'];
-var forceReconfig = true;
+var id = process.env.MRSC_ID;
+var master = process.env.MRSC_MASTER;
+var slaves = process.env.MRSC_SLAVES && process.env.MRSC_SLAVES.split(',');
+var reconfig = process.env.MRSC_RECONFIG && process.env.MRSC_RECONFIG.toLowerCase() === 'true';
+
+if (!id) return console.log('The environment variable MRSC_ID is required.');
+if (!master) return console.log('The environment variable MRSC_MASTER is required.');
+if (!slaves) return console.log('The environment variable MRSC_SLAVES is required.');
 
 connector.open(id, master, slaves)
   .spread(function(db, config) {
-    return configurator.configure(id, db, config, forceReconfig)
+    return configurator.configure(id, db, config, reconfig)
       .then(function() {
         return checker.check(db);
       })
